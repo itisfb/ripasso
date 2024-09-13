e96915037a54d10d2e10ecb4a765f873e87287bf
itisfb <francesco.boccia@itiscuneo.eu>
2024-09-13 13:08:57 +0200

    Create moltiplication of 4

---
+++ es1.py
@@ -0,0 +1,7 @@
+#tabellina del 4 usando range come for: moltiplica 4 per numero di ciclo
+for i in range(11):
+    print(4*i)
+
+#tabellina del 4 usando range(inizio, fine, step)
+for i in range(4,41,4):
+    print(i)
